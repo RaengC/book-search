3243ecb9090b702b3b0752a63cb0f6e6ca09889a
Raeng Castagna <raengc@yahoo.com>
2020-05-31 18:50:05 +1000

    Clean up

--- app.js
+++ app.js
@@ -1,39 +1,24 @@
 
 function queryAPI(search) {
 
-    //make Ajax call to google books (with filters for search term, maximum results and free-ebooks)
     $.ajax({
         url: `https://www.googleapis.com/books/v1/volumes?q=${search}&maxResults=6&filter=ebooks`,
         method: "GET",
     })
         .then((response) => {
-            //console.log(response);
-
-            displayResult(response); //displays result on the DOM
-            // console.log(response.items[0].volumeInfo.title);
-            // console.log(response.items[0].volumeInfo.imageLinks.thumbnail);
-
-            // response.forEach(search => {
-            //     console.log(search.description);
-            // })
+            displayResult(response);
         })
         .catch((error) => console.log(error));
 }
 
-//manipulation on the DOM
+
 function displayResult(result) {
-    //create list of results from 'items' in array to display on DOM
     const list = result.items;
 
-    //console.log(list);
     const bookList = $("#bookSearch");
     bookList.children().remove();
 
-    //looping through the list specifically for title, image links, categories
-    for (let i = 0; i < list.length; i++) {
-        // console.log(list[i].volumeInfo.title);
-        // console.log(list[i].volumeInfo.imageLinks.thumbnail);
-        // console.log(list[i].volumeInfo.categories[0]);
+    for (let i = 0; i < list.length; i++) {);
         bookList.append(`<div class="card shadow" data-toggle="tooltip" data-placement="right" title='${list[i].volumeInfo.description}'>
                             <img src=${list[i].volumeInfo.imageLinks.thumbnail} class="card-img-top shadow-sm"/>
                             <div>
@@ -42,37 +27,24 @@ function displayResult(result) {
                             </div>   
                         </div>`)
 
-        //adding preview
-
         $(".card").draggable();
-
     }
 }
 
 
-
-//on ready
 $(() => {
     console.log('ready');
 
-    //queryAPI();
-
-    // create event listener to displayResult 
     $("#searchBtn").on('click', () => {
-        //console.log("btn clicked");
-
         const search = $("#inp-limit").val();
-
         queryAPI(search);
     })
 
-    //add clear button
+
     $("#clearBtn").on('click', () => {
-        //console.log("clear clicked");
         $('#bookSearch').empty();
     })
 
-    //attempting to clear input on use of clearBtn
     $("#clearBtn").on('click', () => {
         $("#inp-limit").val("");
     })
@@ -81,7 +53,6 @@ $(() => {
         $("#summaryList .card").remove();
     })
     
-    //$(".canDrag").draggable();
 
     $("#summaryList").droppable({
         drop: (event, ui) => {
@@ -103,6 +74,4 @@ $(() => {
             $("#yesList").append(ui.draggable);
         }
     });
-
-
 })
